ac8757c115103299cbb320f173b19d8b0d16edea
parvathy12-tech <parvathypradeep005@gmail.com>
2026-01-08 12:45:07 +0530

    clswrk

--- day8.py
+++ day8.py
@@ -1,4 +1,3 @@
-
 class Person:
     def __init__(self, name, age):
         self.name = name
@@ -7,29 +6,26 @@ class Person:
     def show_details(self):
         print(f"Name: {self.name}, Age: {self.age}")
 
-
 class Employee(Person):
     def __init__(self, name, age, employee_id):
-        super().__init__(name, age)
+        Person.__init__(self, name, age)   
         self.employee_id = employee_id
     
     def show_details(self):
         print(f"Name: {self.name}, Age: {self.age}, Employee ID: {self.employee_id}")
 
-
 class PartTime(Person):
     def __init__(self, name, age, working_hours):
-        super().__init__(name, age)
+        Person.__init__(self, name, age)   
         self.working_hours = working_hours
     
     def show_details(self):
         print(f"Name: {self.name}, Age: {self.age}, Working Hours: {self.working_hours}")
 
-
 class Consultant(Employee, PartTime):
     def __init__(self, name, age, employee_id, working_hours, project_name):
-        Employee.__init__(self, name, age, employee_id)
-        PartTime.__init__(self, name, age, working_hours)
+        Employee.__init__(self, name, age, employee_id)   
+        PartTime.__init__(self, name, age, working_hours) 
         self.project_name = project_name
     
     def show_details(self):
